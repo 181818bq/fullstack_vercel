bf8f74d9c9dd22d4e80646c734dba7b6cea02ba7
Eric <eric_young@epbfi.com>
2025-12-23 16:45:52 -0500

    Defensive checks

--- front-end/src/CommentsList.tsx
+++ front-end/src/CommentsList.tsx
@@ -10,13 +10,23 @@ interface CommentsListProps {
 }
 
 export default function CommentsList({ comments }: CommentsListProps) {
+  // Defensive check: handle undefined, null, or non-array values
+  if (!comments || !Array.isArray(comments) || comments.length === 0) {
+    return (
+      <>
+        <h3>Comments:</h3>
+        <p>No comments yet.</p>
+      </>
+    );
+  }
+
   return (
     <>
       <h3>Comments:</h3>
       {comments.map((comment: Comment, idx: number) => (
         <div key={idx}>
-          <h4>{comment.newComment.postedBy}</h4>
-          <p>{comment.newComment.text}</p>
+          <h4>{comment.newComment?.postedBy || 'Anonymous'}</h4>
+          <p>{comment.newComment?.text || ''}</p>
         </div>
       ))}
     </>
